5cf6617b06557b180e0d10028d956ee73c438939
IASV <ivanantoniosuarezvillalba@gmail.com>
2023-09-11 17:44:43 -0500

    fix: deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Main from '@/views/Main.vue'
 
 //const baseURL = import.meta.env.BASE_URL
-const baseURL = '/'
+const baseURL = '/my-portfolio'
 
 const router = createRouter({
   history: createWebHistory(baseURL),
